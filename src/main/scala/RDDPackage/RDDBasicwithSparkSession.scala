050d8739c7291a8b39fd0c413e18968d63ed6ce3
Rajesh A <a.rajeshinfotech@gmail.com>
2019-06-14 13:09:02 +0100

    Commit 1. To remove the Header of the file 2. Added detailed command for each line for understanding

--- src/main/scala/RDDPackage/RDDBasicwithSparkSession.scala
+++ src/main/scala/RDDPackage/RDDBasicwithSparkSession.scala
@@ -10,21 +10,30 @@ object RDDBasicwithSparkSession {
       .appName("Spark Session Basic")
       .master("local")
       .getOrCreate()
-
+    // Create RDD based on the Arrary Value
     val array = Array(1,2,3,4,5)
     val rdd1 = sparksession.sparkContext.parallelize(array,2)
     rdd1.collect().foreach(println)
 
+    // Load a CSV file
     val filerdd1 = "C:\\Exchange_Summary by Frame._2019_06_13.csv"
     val filerdd2 = sparksession.sparkContext.textFile(filerdd1,3)
     filerdd2.take(2).foreach(println)
+
+    // Seperater in our case its comma seperator
     val filerdd3 = filerdd2.map(y => y.split(","))
-    //filerdd3.take(2).foreach(println)
+    filerdd3.take(5).foreach(println)
+    // select the row that holds the substring "Nivia"
     val filerdd4 = filerdd3.filter(x => x(1).contains("Nivea" ))
     println("Count with Match Char",filerdd4.count())
+    // Select only one based on the sunstring
     val filerdd5 = filerdd4.map(line => line(0))
+    // Select only unique value and sort those values
     filerdd5.distinct.collect().sortBy(x=> x).foreach(println)
-
+    // Remove the header from the file
+    val filehead = filerdd2.first()
+    val filerdd6 = filerdd2.filter(_ != filehead )
+    filerdd6.take(10).foreach(println)
   }
 
 }
