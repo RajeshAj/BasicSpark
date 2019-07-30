4ced9681a5ce1ab6766332492979c2cdd7461230
Rajesh A <a.rajeshinfotech@gmail.com>
2019-07-30 15:48:23 +0100

    Commit Command Line Dataframe - JSON and Parq FileFormat

---
+++ src/main/scala/DFBasic/DFFileFormat.scala
@@ -0,0 +1,25 @@
+package DFBasic
+
+import org.apache.spark.sql.SparkSession
+
+object DFFileFormat {
+  def main(args: Array[String]): Unit = {
+
+    val spark =  SparkSession.builder()
+      .master("local")
+      .appName("DF File Format")
+      .getOrCreate()
+
+    val dfjson = spark.read.json("C:\\TESTJson.json")
+    dfjson.printSchema()
+    println(dfjson.count())
+    dfjson.show(5)
+
+    val dfparq = spark.read.parquet(path="c:\\TESTParq.parquet")
+    dfparq.printSchema()
+    println(dfparq.count())
+    dfparq.show(10)
+
+
+  }
+}
